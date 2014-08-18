73c6e8154e512477ae1bd3ab455fdc1f776a8d37
Anton Tarasenko <antontarasenko@gmail.com>
2014-08-18 22:08:54 +0400

    More structure. The module produce two graphs out of a few-line sample of tweets containing "#ferguson".

--- twitter-nodes/__init__.py
+++ twitter-nodes/__init__.py
@@ -1,39 +1,73 @@
+import re, itertools, sys, os
+import networkx as nx
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+# import pylab as plt
 from operator import itemgetter
 
 __author__ = 'Anton Tarasenko'
-
-import re, itertools, sys, os
-import networkx as nx
-import matplotlib.pyplot as plt
-
-TFILE = os.path.realpath('..') + "/data/raw/test-ferguson.txt"
+FPATH = os.path.realpath('..') + "/data/raw/" # TODO do via sys.*
 
 def main():
     # init
-    msgs = read_raw_stream(TFILE)
+    msgs = load_raw_file("test-ferguson.txt")
+    users, uedges = parse_msgs(msgs, prefix="@")
+    tags, tedges = parse_msgs(msgs, prefix="#")
+    U = create_graph(users, uedges)
+    T = create_graph(tags, tedges)
+    small_plot(U)
+    small_plot(T)
+
+def load_raw_file(file):
+    path = FPATH + file
+    msgs = read_raw_stream(path)
+    return msgs
+
+def parse_msgs(msgs, prefix):
     edges = list()
+    nodes = pd.DataFrame(columns=['name', 'mentions'])
     for msg in msgs:
-        cluster = get_cluster(msg)
+        cluster = get_cluster(msg, prefix=prefix)
         cluster_edges = get_edges(cluster)
         edges = edges + cluster_edges
+
+        # TODO put into the graph
+        isrt = msg[0:3] == "RT "
+        ispos = msg.count(":)") > 0
+        isneg = msg.count(":(") > 0
+
+        for node in cluster:
+            # TODO make nicer
+            if node in nodes['name'].values:
+                ix = nodes['name'] == node
+                nodes.ix[ix, 'mentions'] += nodes.ix[ix, 'mentions']
+            else:
+                nodes = nodes.append({'name': node, 'mentions': 1}, ignore_index=True)
+
     wedges = weight_edges(edges)
 
-    # Report
-    G = create_graph(wedges)
+    return nodes, wedges
+
+def report(G):
     print("Nodes: %s\nEdges: %s" % (len(G.nodes()), len(G.edges())))
     nodes_by_degree = sorted(G.degree_iter(), key=itemgetter(1), reverse=True)
     print("Most connections: %s" % nodes_by_degree[0:5])
 
+def small_plot(G, save=False):
     # Plotting
-    plt.figure(figsize=(10, 10))
+    plt.figure(figsize=(12, 12))
     try:
         pos=nx.graphviz_layout(G)
     except:
         pos=nx.spring_layout(G, iterations=20)
-    nx.draw_networkx_nodes(G, pos, node_color='w', alpha=0.4)
-    nx.draw_networkx_edges(G, pos, alpha=0.4, node_size=0, width=1, edge_color='k')
-    nx.draw_networkx_labels(G, pos, fontsize=14)
-    plt.show();
+    # nx.draw_networkx_nodes(G, pos, node_color='w', alpha=0.4)
+    nx.draw_networkx_edges(G, pos, alpha=0.1, node_size=0, width=1, edge_color='k')
+    nx.draw_networkx_labels(G, pos, fontsize=11)
+    if save:
+        plt.savefig("small_plot.png")
+    else:
+        plt.show()
 
 def read_raw_stream(file):
     with open(file) as f:
@@ -47,7 +81,6 @@ def get_cluster(msg, prefix='#'):
     return cluster
 
 def pairwise(iterable):
-    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
     a, b = itertools.tee(iterable)
     next(b, None)
     return set(a, b)
@@ -57,16 +90,19 @@ def get_edges(cluster):
     return edges
 
 def weight_edges(edges):
-    wedges = set()
-    # TODO Make faster
-    for edge in edges:
-        wedges.add((tuple(edge), edges.count(edge)))
-    return wedges
+    df = pd.DataFrame(edges)
+    df['n'] = 1
+    gb = df.groupby([0, 1])
+    c = gb.count().reset_index()
+    return c
 
-def create_graph(wedges):
+def create_graph(nodes, wedges):
     G = nx.Graph()
-    for wedge in wedges:
-        G.add_edge(wedge[0][0], wedge[0][1], weight=wedge[1])
+    for node in nodes.iterrows():
+        G.add_node(node[1]['name'], mentions = node[1]['mentions'])
+    for wedge in wedges.iterrows():
+        row = wedge[1]
+        G.add_edge(wedge[1][0], wedge[1][1], weight=wedge[1]['n'])
     return G
 
 
